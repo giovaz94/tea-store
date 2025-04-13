1c978f3ba7c9974e0187223eabf1042806f5f2f1
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-04-13 16:47:21 +0200

    feat: multi-cpu service

--- global-scaler/src/components/guard.py
+++ global-scaler/src/components/guard.py
@@ -31,7 +31,7 @@ class Guard:
         self.mixer = mixer
 
         prometheus_service_address = os.environ.get("PROMETHEUS_SERVICE_ADDRESS", "localhost")
-        prometheus_service_port = os.environ.get("PROMETHEUS_SERVICE_PORT", "54361")
+        prometheus_service_port = os.environ.get("PROMETHEUS_SERVICE_PORT", "53216")
         prometheus_url = f"http://{prometheus_service_address}:{prometheus_service_port}"
         self.prometheus_instance = PrometheusConnect(url=prometheus_url)
 
--- service/src/index.ts
+++ service/src/index.ts
@@ -1,13 +1,36 @@
 import express from "express";
+import cluster from "cluster";
+import { cpus } from "os";
 import { handleRequest } from "#middleware/middleware.js";
 import { prometheusMetrics } from "#middleware/prometheus.js";
 
-const app = express();
+const numCPUs = cpus().length;
 const port = process.env.PORT ?? "9001";
 
-app.get("/metrics", prometheusMetrics);
-app.post("/request", handleRequest);
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+  console.log(`Starting ${numCPUs} workers...`);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
+
+  app.get("/metrics", prometheusMetrics);
+  app.post("/request", handleRequest);
+
+  const server = app.listen(port, () => {
+    server.keepAliveTimeout = 65000;
+    server.headersTimeout = 66000;
+    
+    console.log(`Worker ${process.pid} started and listening on port ${port}`);
+  });
+
+  console.log(`Worker ${process.pid} started`);
+}
