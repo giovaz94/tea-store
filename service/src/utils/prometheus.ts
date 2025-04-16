4cce09a4815bb13e60cb43a0d96df685091b1e84
LBacchiani <lorenzo.bacchiani2@unibo.it>
2025-04-16 11:59:44 +0200

    fix(prom metrics)

--- service/src/utils/prometheus.ts
+++ service/src/utils/prometheus.ts
@@ -27,33 +27,41 @@ export function createSimpleCounter(
 export function createIncomingMessageCounter(
   serviceName: string,
 ): Counter<string> {
-  return createCounter(
+  const counter = createCounter(
     `http_requests_total_${serviceName}_counter`,
     `Total number of requests sent to ${serviceName} services`,
     ["service", "status"],
   );
+  counter.inc();
+  return counter;
 }
 
 export function createLostMessageCounter(serviceName: string): Counter<string> {
-  return createCounter(
+  const counter = createCounter(
     `message_lost`,
     "Total number of messages that failed to be delivered",
     ["service", "reason"],
   );
+  counter.inc();
+  return counter;
 }
 
 export function createBehaviourCounter(): Counter<string> {
-  return createCounter(
+  const counter = createCounter(
     "behaviour_execution",
     "Total number of behaviour execution",
     ["service", "reason"],
   );
+  counter.inc();
+  return counter;
 }
 
 export function createBehaviourTimeCounter(): Counter<string> {
-  return createCounter(
+   const counter = createCounter(
     "behaviour_time_execution",
     "Time needed to execute the behaviour",
     ["service", "reason"],
   );
+  counter.inc();
+  return counter;
 }
