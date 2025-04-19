2977addb9854d69d485ce1dc8d2e9052566986c6
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2025-04-19 09:56:14 +0200

    fix: remove filter for istio messages

--- service/src/index.ts
+++ service/src/index.ts
@@ -33,21 +33,6 @@ const outputServices: Map<string, string> = new Map(Object.entries(JSON.parse(pr
 const requestQueue: Task[] = [];
 let runningTasks = 0;
 
-function isIstioRequest(req: Request) {
-  return (
-    req.headers['x-istio-attributes'] !== undefined || 
-    req.headers['x-envoy-attempt-count'] !== undefined ||
-    req.headers['x-request-id']?.includes('istio') ||
-    req.headers['x-envoy-decorator-operation'] !== undefined ||
-    req.headers['x-envoy-peer-metadata'] !== undefined
-  );
-}
-function filterIstioMiddleware(req: Request, res: Response, next: NextFunction) {
-  if (isIstioRequest(req)) {
-    return res.sendStatus(200);
-  }
-  next();
-}
 
 
 function rateLimitMiddleware(req: Request, res: Response, next: NextFunction) {
@@ -86,8 +71,6 @@ function rateLimitMiddleware(req: Request, res: Response, next: NextFunction) {
 const app = express();
 const port = process.env.PORT ?? "9001";
 
-//@ts-ignore
-app.use(filterIstioMiddleware);
 app.get("/metrics", prometheusMetrics);
 if(serviceName !== "recommender") {  
   app.post("/request", rateLimitMiddleware);
